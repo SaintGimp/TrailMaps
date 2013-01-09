591b697e2fb91d011a1fd5014c92842ba3d56030
Eric Lee <saintgimp@hotmail.com>
2013-01-08 23:24:59 -0800

    First stab at express/angular template

---
+++ app.js
@@ -0,0 +1,54 @@
+
+// http://brianstoner.com/blog/testing-in-nodejs-with-mocha/
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+  routes = require('./routes'),
+  api = require('./routes/api'),
+  path = require('path');
+
+var app = module.exports = express();
+
+// Configuration
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.logger('dev'));
+  app.use(express.favicon());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// JSON API
+
+app.get('/api/name', api.name);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+// Start server
+
+app.listen(app.get('port'), function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
