cf015b29e3a66e32ef95454a5c3ed0cb2148b38e
Eric Lee <saintgimp@hotmail.com>
2013-01-10 01:00:38 -0800

    Add new version of grunt

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+module.exports = function (grunt) {
+    "use strict";
+
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: { src: ["Gruntfile.js", "app.js", "**/*.js", "!public/js/lib/**/*.js", "!node_modules/**/*.js"] },
+            options: {
+                jshintrc: ".jshintrc"
+            }
+        },
+        simplemocha: {
+            all: { src: 'test/**/*.js' },
+            options: {
+                globals: ['should'],
+                timeout: 3000,
+                ignoreLeaks: false,
+                grep: '*-test',
+                ui: 'bdd',
+                reporter: 'dot'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-simple-mocha');
+
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'simplemocha']);
+};
