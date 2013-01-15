52a570490650d32fe5e347ace9bb606e9120f24c
Eric Lee <saintgimp@hotmail.com>
2013-01-14 17:37:36 -0800

    Filter track data based on location

--- test/track.test.js
+++ test/track.test.js
@@ -14,7 +14,7 @@ describe('Getting track data', function() {
     });
 
     it('should return the entire track', function() {
-      data.length.should.equal(10);
+      data.should.have.length(10);
     });
   });
 
@@ -23,19 +23,35 @@ describe('Getting track data', function() {
       data = track.getPoints({north: 32.590200, south: 32.590100, east: -116.467300, west: -116.467600, zoom: 15});
     });
 
-    it('should return only part of the track', function() {
-      data.length.should.equal(2);
+    it('should return part of the track', function() {
+      data.should.have.length(2);
     });
 
     it('should filter out north/south data', function() {
-      data.forEach(function(point){
+      data.forEach(function(point) {
         point.lat.should.be.within(32.590100, 32.590200);
       });
     });
 
     it('should filter out east/west data', function() {
-      data.forEach(function(point){
+      data.forEach(function(point) {
         point.lon.should.be.within(-116.467600, -116.467300);
+      });
+    });
+  });
+
+  describe('when requesting data for a zoom level', function() {
+    before(function() {
+      data = track.getPoints({north: 33, south: 32, east: -116, west: -117, zoom: 13});
+    });
+
+    it('should return part of the track', function() {
+      data.should.have.length(3);
+    });
+
+    it('should filter out higher zoom level data', function() {
+      data.forEach(function(point) {
+        point.zoom.should.not.be.above(13);
       });
     });
   });
