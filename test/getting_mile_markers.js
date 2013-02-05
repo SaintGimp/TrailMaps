4a1f003c25771dcc51597973b082182f7a4426ac
Eric Lee <saintgimp@hotmail.com>
2013-02-04 17:55:46 -0800

    Fix unit tests

--- test/getting_mile_markers.js
+++ test/getting_mile_markers.js
@@ -1,21 +1,21 @@
 var should = require('should');
 var fakeDataService = require('./fakeDataService');
-var waypoints = require('../domain/waypoints')(fakeDataService);
+var mileMarkers = require('../domain/milemarkers')(fakeDataService);
 
-describe('Finding waypoints by area', function() {
-  var waypointData;
+describe('Finding mile markers by area', function() {
+  var mileMarkerData;
 
-  describe('when finding waypoints', function() {
+  describe('when finding mile markers', function() {
     before(function(done){
       var options = {trailName: "pct", north: 50, south: 32, east: -110, west: -125, detailLevel: 5};
-      waypoints.findByArea(options, function(err, data) {
-        waypointData = data;
+      mileMarkers.findByArea(options, function(err, data) {
+        mileMarkerData = data;
         done();
       });
     });
 
-    it('should get waypoint data from the service', function() {
-      waypointData.should.have.length(2);
+    it('should get mile marker data from the service', function() {
+      mileMarkerData.should.have.length(2);
     });
 
     it('should get data from the collection corresponding to the trail name', function() {
@@ -37,13 +37,13 @@ describe('Finding waypoints by area', function() {
   describe('when requesting more than the maximum detail', function() {
     before(function(done){
       var options = {trailName: "pct", north: 50, south: 32, east: -110, west: -125, detailLevel: 20};
-      waypoints.findByArea(options, function(err, data) {
-        waypointData = data;
+      mileMarkers.findByArea(options, function(err, data) {
+        mileMarkerData = data;
         done();
       });
     });
 
-    it('should get waypoints from the the collection corresponding to the maximum available detail level', function() {
+    it('should get mile markers from the the collection corresponding to the maximum available detail level', function() {
       fakeDataService.getLastCall().collectionName.should.match(/.*14$/);
     });
   });
@@ -54,7 +54,7 @@ describe('Finding waypoints by area', function() {
     before(function(done){
       fakeDataService.shouldErrorOnNextCall = true;
       var options = {trailName: "pct", north: 50, south: 32, east: -110, west: -125, detailLevel: 20};
-      waypoints.findByArea(options, function(err, data) {
+      mileMarkers.findByArea(options, function(err, data) {
         errorFromCall = err;
         done();
       });
@@ -66,31 +66,31 @@ describe('Finding waypoints by area', function() {
   });
 });
 
-describe('Finding a waypoint by value', function() {
-  var foundWaypoint;
+describe('Finding a mile marker by value', function() {
+  var foundMileMarker;
 
-  describe('when finding a waypoint', function() {
+  describe('when finding a mile marker', function() {
     before(function(done){
       var options = {trailName: "pct", mile: 1234};
-      waypoints.findByValue(options, function(err, waypoint) {
-        foundWaypoint = waypoint;
+      mileMarkers.findByValue(options, function(err, mileMarker) {
+        foundMileMarker = mileMarker;
         done();
       });
     });
 
-    it('should get a waypoint from the service', function() {
-      should.exist(foundWaypoint);
+    it('should get a mile marker from the service', function() {
+      should.exist(foundMileMarker);
     });
 
-    it('should get the waypoint from the collection corresponding to the trail name', function() {
+    it('should get the mile marker from the collection corresponding to the trail name', function() {
       fakeDataService.getLastCall().collectionName.should.match(/^pct_milemarkers\d+$/);
     });
 
-    it('should get the waypoint from the maximum detail level collection', function() {
+    it('should get the mile marker from the maximum detail level collection', function() {
       fakeDataService.getLastCall().collectionName.should.match(/.*14$/);
     });
 
-    it('should get the waypoint by value', function() {
+    it('should get the mile marker by value', function() {
       fakeDataService.getLastCall().searchTerms.mile.should.equal(1234);
     });
   });
