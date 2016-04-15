2977acfd321c44726c0300ccb2feb98ddb23cddc
Eric Lee <saintgimp@hotmail.com>
2016-04-14 17:41:57 -0700

    Fix tests and add files for VS Code

--- domain/tracks.js
+++ domain/tracks.js
@@ -25,11 +25,13 @@ exports.findByArea = function(options) {
   var projection = { _id: 0, loc: 1 };
   var sortOrder = { seq: 1 };
 
-  return stopwatch()(function() {
+  var stopwatchPromise = stopwatch()(function() {
     return dataService.findArray(collectionName, searchTerms, projection, sortOrder);
   })()
   .then(function(response){
     console.log('Track load took ' + response.duration + 'ms');
     return response.result;
   });
+  
+  return new Q(stopwatchPromise);
 };
